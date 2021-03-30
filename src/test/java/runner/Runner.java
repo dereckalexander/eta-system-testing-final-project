8a8afc96660ec08d9b3b33bc5a4d7c10a977c907
Thamires Neves <thamires_neves@apple.com>
2021-03-30 14:44:14 -0300

    activity/creating-project-environment

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,19 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/resources/features",
+        glue = "step-definitions",
+        tags = "",
+        monochrome = false,
+        plugin = {"pretty"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+public class Runner {
+
+
+}
