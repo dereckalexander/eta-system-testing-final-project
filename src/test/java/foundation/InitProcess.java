db0206af262f1ed29727b7498268a7f47b31cc11
dereckalexander <dereck.portela@liferay.com>
2021-04-02 10:51:57 -0300

    Add init, new chrome driver and pom.xml

---
+++ src/test/java/foundation/InitProcess.java
@@ -0,0 +1,20 @@
+
+package foundation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class InitProcess {
+    public static WebDriver createBrowser() {
+        String airbnbUrl = "https://www.airbnb.com.br/";
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(airbnbUrl);
+        return driver;
+    }
+}
